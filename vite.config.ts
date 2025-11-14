6e835e431d557476fb37d1d046e377162129ab25
Leomaartin <leonelmartin9808@gmail.com>
2025-11-14 15:12:21 -0300

    Permitir host de Render en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // asegura que los paths funcionen en producción
+  base: "/",
   server: {
     port: 5173,
+  },
+  preview: {
+    allowedHosts: ["utnproyectoreact-4.onrender.com"],
   },
 });
