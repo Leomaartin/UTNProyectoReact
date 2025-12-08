d043757c2ae4c97df323f9ec91034afdffe66ec5
Leomaartin <leonelmartin9808@gmail.com>
2025-12-07 23:19:01 -0300

    Ultimo commit antes de hostear

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, Toaster } from "react-hot-toast";
 import axios from "axios";
+import { Card } from "../Components/Card";
 
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
